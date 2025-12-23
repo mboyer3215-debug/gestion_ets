c3e260f4bc51f855b373cf02c67998e4b39b3561
mboyer3215-debug <m.boyer3215@gmail.com>
2025-12-23 01:09:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1694,7 +1694,6 @@ def prestation_nouvelle():
 
 @app.route('/prestation/<int:prestation_id>/modifier', methods=['GET', 'POST'])
 def prestation_modifier(prestation_id):
-    return prestation_modifier(prestation_id)
     """Modifier une prestation"""
     prestation = Prestation.query.get_or_404(prestation_id)
 
@@ -5581,6 +5580,7 @@ with app.app_context():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)       
+
 
 
 
